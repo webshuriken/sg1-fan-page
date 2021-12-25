d1817eed0fb8f4f322134ec9eeea41feb628997f
Carlos E Alford M <carlos.alford@gmail.com>
2021-12-25 21:22:33 +0000

    feat: Mobile menu closes on user clicks when open

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 function initMobileMenu() {
   // get the mobile menu button
   const burgerBtn = document.querySelector('#burger-btn');
+  // get navlist to close nav on click
+  const navList = document.querySelector('#nav-list');
   // listen for clicks on the mobile menu button
   burgerBtn.addEventListener('click', elem => {
     elem.preventDefault();
-    // get nav list
-    const navList = document.querySelector('#nav-list');
+    // toggle nav list
+    navList.classList.toggle('active');
+  });
+  // listen to all clicks on the nav list, disable on screens < 768px
+  navList.addEventListener('click', elem => {
+    // toggle navlist
     navList.classList.toggle('active');
   });
 }
