fe38a231fd9e98549d5e30f649c882a4d33a6104
Carlos E Alford M <carlos.alford@gmail.com>
2021-12-27 22:28:27 +0000

    feat: Create smooth endless carousel routine

--- main.js
+++ main.js
@@ -28,5 +28,46 @@ function initCardFlip() {
   })
 }
 
+/**
+ * @description Takes care of the vehicle slider
+ */
+function initSlides() {
+  // enclosed to keep track of visible slide
+  let activeSlide = 0;
+  // get left and right buttons
+  const slideBtns = document.querySelectorAll('.vehicles-slide__btns');
+  // moves the slides left or right, uses enclosure to keep track
+  function moveSlide(ele) {
+    // get slide direction from clicked button
+    const direction = ele.srcElement.dataset.slide
+    const slides = document.querySelectorAll('.vehicles-slide')
+    const slidesLength = slides.length - 1;
+    // slides will move either left or right only, dont accept other values
+    if (direction === 'right') {
+      // slide from right to left
+      slides[activeSlide].style.animationName="hideSlideRight";
+      activeSlide++;
+      // keep within available slides bound
+      if (activeSlide > slidesLength) {activeSlide = 0}
+      slides[activeSlide].style.animationName="slideFromRight";
+    }else if (direction === 'left') {
+      // slide from left to right
+      slides[activeSlide].style.animationName="hideSlideLeft";
+      activeSlide--;
+      // keep within available slides bound
+      if (activeSlide < 0) {activeSlide = slidesLength}
+      slides[activeSlide].style.animationName="slideFromLeft";
+    }else {
+      console.error('Slides: Is trying to slide without buttons');
+    }
+  }
+  // listen for btn click
+  slideBtns.forEach(btn => {
+    btn.addEventListener('click', moveSlide);
+  });
+}
+
+// start all methods for site interactivity
 initMobileMenu();
 initCardFlip();
+initSlides();
